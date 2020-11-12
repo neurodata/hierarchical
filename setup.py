d926bdf9631656205d6348dffff6339d64ac35f3
hhelm10 <31412718+hhelm10@users.noreply.github.com>
2020-11-12 09:02:01 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,5 @@
 from setuptools import setup, find_packages
 
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
-
-from tasksim import task_similarity
-
-from graspy.embed import AdjacencySpectralEmbed as ASE
-from graspy.cluster import AutoGMMCluster as GMM
-
-from proglearn import LifelongClassificationForest as l2f
-import torch
-import torchvision
-import torchvision.transforms as transforms
-import torchvision.models as models
-
-from graspy.embed import ClassicalMDS as CMDS
-
-import time
-from tqdm import tqdm
-
-from joblib import Parallel, delayed
-
-from sklearn.metrics import normalized_mutual_info_score as NMI
-from sklearn.metrics import pairwise_distances
-
 requirements = [
     "graspy",
     "proglearn",
